9c71b6fec87333a3c050ff50d092a3d0140d2cf9
KaiserEMP <kaidogta3@hotmail.com>
2014-10-27 13:28:16 +0200

    Finisshheeeed
    some deletes

--- validation.php
+++ validation.php
@@ -17,33 +17,4 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-	$isSubmitted = isset($_POST['submit']);
-
-	if ($isSubmitted) {
-		$username = $_POST['username'];
-
-	}
-
-	if (!isset($username)) || $username == "") {
-		$usernameMessage = "Palun sisestada kasutajanimi";
-
- 	}
- 	else {
- 		$usernameMessage = "kasutajanimi sobis";
- 	}
-
-*/
  ?>
